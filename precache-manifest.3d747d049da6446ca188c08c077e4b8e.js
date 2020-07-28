3d54abc77b7005506eed8407a1afea0d6df4e505
3rfanian <hooman.erfan@mail.polimi.it>
2020-07-28 15:53:42 +0430

    Updates

---
+++ precache-manifest.3d747d049da6446ca188c08c077e4b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4df01e7446fa1e9f799bb5fd964a825",
+    "url": "/GOL-game-of-life/index.html"
+  },
+  {
+    "revision": "720c0549a6645ef62550",
+    "url": "/GOL-game-of-life/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02705d0d42a88a3bd173",
+    "url": "/GOL-game-of-life/static/js/2.5ae7e007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GOL-game-of-life/static/js/2.5ae7e007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720c0549a6645ef62550",
+    "url": "/GOL-game-of-life/static/js/main.2ff92bcb.chunk.js"
+  },
+  {
+    "revision": "885c6e783a1581c29eb3",
+    "url": "/GOL-game-of-life/static/js/runtime-main.d47e7c04.js"
+  }
+]);
